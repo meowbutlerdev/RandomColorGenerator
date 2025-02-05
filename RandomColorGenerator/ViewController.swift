39ec194b1f0ef59aa4252e19f14725dcb43bf4b3
JISUNG PARK <meowbutlerdev@gmail.com>
2025-02-05 16:26:16 +0900

    feat: 랜덤 컬러 생성
    - 랜덤 색상을 생성하는 함수 정의
    - 랜덤 색상으로 배경 색상을 변경하는 버튼 구현

--- RandomColorGenerator/ViewController.swift
+++ RandomColorGenerator/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var backgroundColorView: UIView!
+    
+    @IBAction func changeColorTapped(_ sender: UIButton) {
+        backgroundColorView.backgroundColor = generateRandomColor()
+    }
+    
+    func generateRandomColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
