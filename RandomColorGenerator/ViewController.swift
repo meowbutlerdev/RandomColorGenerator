7012b016ddfd7516f76d591933a58d359daa3eaf
JISUNG PARK <meowbutlerdev@gmail.com>
2025-02-05 15:50:53 +0900

    Initial Commit

---
+++ RandomColorGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColorGenerator
+//
+//  Created by 박지성 on 2/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RandomColorGeneratorTests/RandomColorGeneratorTests.swift
@@ -0,0 +1,17 @@
+//
+//  RandomColorGeneratorTests.swift
+//  RandomColorGeneratorTests
+//
+//  Created by 박지성 on 2/5/25.
+//
+
+import Testing
+@testable import RandomColorGenerator
+
+struct RandomColorGeneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
